f2742d7b20d24d2f7261733b491db81070318431
at//design <mail@at-world.de>
2024-03-20 17:30:11 +0100

    Removed tests

--- Tests/NGFirebaseConfigurationTests/FirebaseConfigurationTests.swift
+++ Tests/NGFirebaseConfigurationTests/FirebaseConfigurationTests.swift
@@ -1,12 +1,6 @@
 import XCTest
-@testable import FirebaseConfiguration
+@testable import NGFirebaseConfiguration
 
 final class FirebaseConfigurationTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
 }
