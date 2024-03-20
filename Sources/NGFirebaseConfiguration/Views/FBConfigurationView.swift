9457afec2d75ebe8c48f08180a067e8c528fccce
at//design <mail@at-world.de>
2024-03-20 18:19:53 +0100

    Changed initializer store type
    Store is now a @Binding

--- Sources/NGFirebaseConfiguration/Views/FBConfigurationView.swift
+++ Sources/NGFirebaseConfiguration/Views/FBConfigurationView.swift
@@ -12,16 +12,16 @@ import Observation
 ///
 /// Use this view to wrap it around your base content view or in #preview for testing
 public struct FBConfigurationView<Content, Store: AnyObject>: View  where Content: View, Store: Observable {
-  @Bindable var store: Store
+  @Binding var store: Store
   private let content: () -> Content
   /// Creates a new FBConfigurationView
   /// - Parameters:
-  ///   - store: Your data container
+  ///   - store: Your data container. It is injected into the enviroment. So don't inject it yourself!!!
   ///   - options: Set the Firebase options
   ///   - useEmulator: Select if you want to use the emulator
   ///   - server: Sets the server for your Firebase project
   ///   - content: The view content
-  public init(store: Bindable<Store>,
+  public init(store: Binding<Store>,
               options: FBOptions,
               useEmulator: Bool = false,
               emulatorOptions: FBEmulatorOptions = .defaultOptions(), @ViewBuilder content: @escaping () -> Content) {
