eaaa652af402771e7fbc79d42734ddbc0b9a6b52
at//design <mail@at-world.de>
2024-03-30 14:29:25 +0100

    Added Logger to FBConfiguration

--- Sources/NGFirebaseConfiguration/FBConfiguration.swift
+++ Sources/NGFirebaseConfiguration/FBConfiguration.swift
@@ -10,11 +10,13 @@ import Firebase
 import FirebaseStorage
 import FirebaseFunctions
 import FirebaseDatabase
+import OSLog
 
 /// Firebase configuration
 ///
 /// An easy way to start your Firebase project without a AppDelegate.
 public struct FBConfiguration {
+  private static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "FBConfiguration")
   /// Creates a new FBConfiguration with FBOptions and emulator option.
   /// - Parameters:
   ///   - options: Firebase project options
@@ -27,12 +29,15 @@ public struct FBConfiguration {
       FirebaseApp.configure(options: .init(options: options))
       // Emulator configuration
       if useEmulator {
+        logger.info("use firebase emulator")
         // Auth emulator configuration
         if let authPort = emulatorOptions.authPort {
+          logger.info("use firebase auth with emulator")
           Auth.auth().useEmulator(withHost: emulatorOptions.server, port: authPort)
         }
         // Firestore emulator configuration
         if let firestorePort = emulatorOptions.firestorePort {
+          logger.info("use firebase firestore with emulator")
           Firestore.firestore().useEmulator(withHost: emulatorOptions.server, port: firestorePort)
           let settings = Firestore.firestore().settings
           settings.cacheSettings = MemoryCacheSettings()
@@ -41,9 +46,11 @@ public struct FBConfiguration {
         }
         // Storage emulator configuration
         if let storagePort = emulatorOptions.storagePort {
+          logger.info("use firebase storage with emulator")
           Storage.storage().useEmulator(withHost: emulatorOptions.server, port: storagePort)
         }
         if let functionsPort = emulatorOptions.functionsPort {
+          logger.info("use firebase functions with emulator")
           Functions.functions().useEmulator(withHost: emulatorOptions.server, port: functionsPort)
         }
       }
